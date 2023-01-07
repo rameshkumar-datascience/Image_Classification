c776379ebd2f3fc31e59bca62b54b540f55962f7
rameshmkumar-datascience <rameshmangali947@gmail.com>
2023-01-07 09:46:07 +0530

    setup edited

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Image_Classification"
+REPO_NAME = "Image_Classifier"
 AUTHOR_USER_NAME = "rameshkumar-datascience"
 SRC_REPO = "Image_Classification"
 AUTHOR_EMAIL = "rameshmangali947@gmail.com"
